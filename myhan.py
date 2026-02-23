6509f9cf7afa7981bc13b0f63ff784565a83f2fd
hannahalmuena-lang <hannahalmuena@gmail.com>
2026-02-23 23:53:52 +0800

    Add files via upload

---
+++ myhan.py
@@ -0,0 +1,34 @@
+def myhan():
+    print("SELECT OPERATION:")
+    print("\n[1]Add")
+    print("\n[2]Subtract")
+    print("\n[3]Multiply")
+    print("\n[4]Divide\n")
+
+user = int(input("Enter Choices: ")) 
+if user == 1:
+    a = int(input("enter 1st number: "))
+    b = int(input("enter 2nd number: "))
+    result = a+b
+    print("Result is: ",result)
+
+elif user == 2:
+    a = int(input("enter 1st number: "))
+    b = int(input("enter 2nd number; "))
+    result = a-b
+    print("Result is: ",result)
+
+elif user == 3:
+    a = int(input("enter 1st number: "))
+    b = int(input("enter 2nd number: "))
+    result = a*b
+    print("Result is: ",result)
+
+elif user == 4:
+    a = int(input("enter 1st number: "))
+    b = int(input("enter 2nd number: "))
+    result = a/b
+    print("Result is: ",result)
+else:
+    print("wrong password")
+myhan()
